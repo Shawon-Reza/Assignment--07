d27726ed4b39e276e0e6b3b8d646e49a0d132c94
Shawon Reza <shawon.reza.sr81@gmail.com>
2024-11-12 16:56:10 +0600

    toggle Btn

--- src/Components/AllPlayers/AllPlayers.jsx
+++ src/Components/AllPlayers/AllPlayers.jsx
@@ -2,20 +2,38 @@ import { useState } from "react";
 
 const AllPlayers = () => {
 
-const [activeStatuse,setActiveStatuse]=useState(true)
+    const [activetbn, setActivetbn] = useState({
+        cart: true,
+        status: "cart"
+    });
+    console.log(activetbn)
 
-const handleActiveButton=()=>{
-    setActiveStatuse(!activeStatuse)
-}
-    
+    const handleActiveButton = (status) => {
+        if (status == "cart") {
+            setActivetbn({
+                cart: true,
+                status: "cart"
+            })
+        }
+        else{
+            setActivetbn({
+                cart: false,
+                status: "selected"
+            })
+        }
+    }
+
+
     return (
         <div className="container mx-auto mt-10">
             <div className="flex justify-between items-center">
                 <h3 className="text-3xl font-bold">Available Players</h3>
                 <div className=" space-x-3">
-                <button className="btn">Available</button>
-                <button className="btn">Selected</button>
+                    {/* Acative Button */}
+                    <button className={`${activetbn.cart ? "btn bg-orange-300" : "btn"}`} onClick={()=>{handleActiveButton("cart")}}>Available</button>
+                    <button className={`${activetbn.cart ? "btn" : "btn bg-orange-300"}`} onClick={()=>{handleActiveButton("selected")}}>Selected</button>
                 </div>
+
             </div>
             <div>
 
