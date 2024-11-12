e14077e364fa81d382966d0065de758d19b2f235
Shawon Reza <shawon.reza.sr81@gmail.com>
2024-11-12 17:14:20 +0600

    Fetch data

--- src/Components/AllPlayers/AllPlayers.jsx
+++ src/Components/AllPlayers/AllPlayers.jsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const AllPlayers = () => {
-
+    // Activ Button State
     const [activetbn, setActivetbn] = useState({
         cart: true,
         status: "cart"
     });
-    console.log(activetbn)
-
+    // Active Button Status HAndle
     const handleActiveButton = (status) => {
         if (status == "cart") {
             setActivetbn({
@@ -15,26 +14,48 @@ const AllPlayers = () => {
                 status: "cart"
             })
         }
-        else{
+        else {
             setActivetbn({
                 cart: false,
                 status: "selected"
             })
         }
     }
+    // State For All Players data 
+    const [allPlayers, setAllPlayers] = useState([])  //All players Details
+    console.log(allPlayers)
+    //UseEffect for Fetch Facked Data For All Players.
+
+    useEffect(() => {
+        fetch('../../../public/Deta.json')
+            .then(data => data.json())
+            .then(res => setAllPlayers(res))
+            .catch(error=>{"Error occur while Fetching ALl players Details",error})
+    }, [])
+
+
+
+
 
 
     return (
         <div className="container mx-auto mt-10">
+            {/* Active Button Container Start*/}
             <div className="flex justify-between items-center">
                 <h3 className="text-3xl font-bold">Available Players</h3>
                 <div className=" space-x-3">
                     {/* Acative Button */}
-                    <button className={`${activetbn.cart ? "btn bg-orange-300" : "btn"}`} onClick={()=>{handleActiveButton("cart")}}>Available</button>
-                    <button className={`${activetbn.cart ? "btn" : "btn bg-orange-300"}`} onClick={()=>{handleActiveButton("selected")}}>Selected</button>
-                </div>
-
+                    <button className={`${activetbn.cart ? "btn bg-orange-300" : "btn"}`} onClick={() => { handleActiveButton("cart") }}>Available</button>
+                    <button className={`${activetbn.cart ? "btn" : "btn bg-orange-300"}`} onClick={() => { handleActiveButton("selected") }}>Selected</button>
+                </div> {/* Active Button Container End */}
             </div>
+
+            {/* All Players Container Start */}
+
+
+
+
+            {/* All Players Container End */}
             <div>
 
             </div>
