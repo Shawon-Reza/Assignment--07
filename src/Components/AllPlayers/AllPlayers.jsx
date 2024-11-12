532194cb524318976bdb5a49d58435a4f8942174
Shawon Reza <shawon.reza.sr81@gmail.com>
2024-11-13 00:16:38 +0600

    Toggle Button work with funtionalities

--- src/Components/AllPlayers/AllPlayers.jsx
+++ src/Components/AllPlayers/AllPlayers.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import SinglePlayer from "../singlePlayer/singlePlayer";
 import { text } from "@fortawesome/fontawesome-svg-core";
 import { getStoredPlayers } from "../LocalStorage/Localstorage";
+import Selected from "../Selected/Selected";
 
 
 
@@ -56,10 +57,10 @@ const AllPlayers = ({ handleCOinAfterPurchase }) => {
     };
 
     // Local Storage Part
-    const [storedplayers,setStoredPlayers]=useState([])  //storedplayers= localStorage Saved Data
+    const [storedplayers, setStoredPlayers] = useState([])  //storedplayers= localStorage Saved Data
     useEffect(() => {
         const LsPlayersId = getStoredPlayers()  //Localstorage Players ID
-        
+
         const StoredPlayerslist = []
         for (const id of LsPlayersId) {
             const Player = allPlayers.find(p => p.ID == id)
@@ -68,9 +69,9 @@ const AllPlayers = ({ handleCOinAfterPurchase }) => {
                 // console.log("Stored",StoredPlayerslist);
                 setStoredPlayers(StoredPlayerslist)
             }
-        } 
-    }, [selectedPlayers,allPlayers])
-    console.log("LS Saved Players",storedplayers);
+        }
+    }, [selectedPlayers, allPlayers])
+    console.log("LS Saved Players", storedplayers);
 
 
 
@@ -82,26 +83,45 @@ const AllPlayers = ({ handleCOinAfterPurchase }) => {
             {/* Active Button Container Start*/}
             <div className="flex justify-between items-center mb-3">
                 <h3 className="text-3xl font-bold">Available Players</h3>
+
                 <div className=" space-x-3">
                     {/* Acative Button */}
                     <button className={`${activetbn.cart ? "btn bg-orange-300" : "btn"}`} onClick={() => { handleActiveButton("cart") }}>Available</button>
+
                     <button className={`${activetbn.cart ? "btn" : "btn bg-orange-300"}`} onClick={() => { handleActiveButton("selected") }}>Selected</button>
                 </div> {/* Active Button Container End */}
+
             </div>
 
             <div>
                 {/* All Players Container Start : Dynamically Sshow */}
 
-                <div className="grid grid-cols-2 gap-3">
-                    {
-                        allPlayers.map((player, idx) => <SinglePlayer
-                            key={idx}
-                            player={player}
-                            handlePlayerSelect={handlePlayerSelect}
-                            handleCOinAfterPurchase={handleCOinAfterPurchase}
-                        ></SinglePlayer>)
-                    }
-                </div>
+                {
+                    activetbn.cart ? (
+                        <div className="grid grid-cols-2 gap-3">
+                            {
+                                allPlayers.map((player, idx) => <SinglePlayer
+                                    key={idx}
+                                    player={player}
+                                    handlePlayerSelect={handlePlayerSelect}
+                                    handleCOinAfterPurchase={handleCOinAfterPurchase}
+                                ></SinglePlayer>)
+                            }
+                        </div>)
+                        :
+                        (
+                            <div className="">
+                                {
+                                    storedplayers.map((player, idx) => <Selected
+                                        key={idx}
+                                        LSplayersData={player}
+                                    ></Selected>)
+                                }
+                            </div>)
+                }
+
+
+
 
 
 
---
+++ src/Components/Selected/Selected.jsx
@@ -0,0 +1,10 @@
+
+const Selected = () => {
+    return (
+        <div>
+            <h1>Selected Part</h1>
+        </div>
+    );
+};
+
+export default Selected;
